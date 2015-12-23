f7a28f1bf0a8e399edb80c5eec8b13a1cfc9b909
Anton Frolovsky <antonfrolovsky@gmail.com>
2015-12-23 18:06:09 +0400

    Update calendar

--- scripts/angular/rez-trip.js
+++ scripts/angular/rez-trip.js
@@ -10,6 +10,32 @@
       defaultLocale: 'en',
       defaultCurrency: 'USD'
     }))
-    .controller('main', function($scope) {
-    });
+    .controller('bookingWidget', [function() {
+      var self = this;
+
+      this.arrival = null;
+      this.departure = null;
+
+      this.arrivalOptions = {
+        minDate: 0
+      }
+
+      this.departureOptions = {
+        minDate: 0
+      }
+
+      this.chachgeMinDate = function(target) {
+
+        if (target == 'arrival') {
+          self.arrivalOptions = {
+            minDate: 0
+          }
+        } else {
+          self.departureOptions = {
+            minDate: 0
+          }
+        }
+
+      }
+    }]);
 })();
