6e2f1f5f62978976500c7b06b74ffd7dd0b68e41
Anton Frolovsky <antonfrolovsky@gmail.com>
2015-12-25 17:46:15 +0400

    Tonight's rate

--- scripts/angular/rez-trip.js
+++ scripts/angular/rez-trip.js
@@ -10,26 +10,38 @@
       defaultLocale: 'en',
       defaultCurrency: 'USD'
     }))
+    .directive('onSearchChanged', function (rt3Search) {
+      return {
+        scope: false,
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+          scope.$watch('search.params', function (params) {
+            if (params.arrival_date && params.departure_date) {
+              scope.$eval(attrs.onSearchChanged);
+            }
+          }, true);
+
+          scope.$eval(attrs.onSearchChanged);
+        }
+      };
+    })
     .controller('bookingWidget', ['$scope', function($scope) {
       var self = this;
 
-      this.arrival = null;
-      this.departure = null;
       this.arrivalOptions = {
         minDate: 0,
       }
       this.departureOptions = {
         minDate: 0
       }
-
+      // Todo move to service
       this.chachgeMinDate = function(target) {
         var today = new Date().getDate();
-        var arr = new Date(self.arrival).getDate();
-        var dep = new Date(self.departure).getDate();
+        var arr = new Date($scope.search.params.arrival_date).getDate();
+        var dep = new Date($scope.search.params.departure_date).getDate();
 
         if (target == 'departure') {
           self.departureOptions.minDate = (arr - today) + 1;
-          // self.departure = TODO
         }
       }
     }]);
