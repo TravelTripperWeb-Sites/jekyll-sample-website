a9bf15af4f446713a1db73cab4956893c48470e2
Anton Frolovsky <antonfrolovsky@gmail.com>
2016-01-21 18:44:52 +0400

    Move scripts to assets

---
+++ _assets/javascripts/application.js
@@ -0,0 +1,13 @@
+//= require libs/jquery.min
+//= require libs/jquery-ui.min
+//= require angular/angular.min
+//= require libs/ui-date
+//= require libs/rt3api
+
+//= require angular/rez-trip
+//= require angular/rt3-services
+//= require angular/rt3-directives
+//= require angular/rt3-filters
+
+//= require user-script
+//= require booking-widget
